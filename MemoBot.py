6045f73563767dfa5b2a2e97b500a1ede4c96cfa
Chase Walters <memotexe@gmail.com>
2021-07-30 22:12:58 -0400

    Music Feature Started

--- MemoBot.py
+++ MemoBot.py
@@ -7,6 +7,7 @@ import time
 import asyncio
 import time
 from itertools import cycle
+import youtube_dl
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
@@ -15,7 +16,6 @@ status = ["Hello! I'm MemoBot!", "Type ?Help For Commands!"]
 
 @bot.event
 async def on_ready():
-    #await bot.change_presence(activity=discord.Game('Type ?Help For Commands'))
     print("This lil Bob-omb is ready to assist!")
     bot.loop.create_task(change_status())
 
@@ -30,5 +30,73 @@ async def change_status():
 @bot.command(name='Help')
 async def help_command(ctx):
     await ctx.channel.send("Help")
+
+
+
+@bot.command()
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'Stop' Command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Bot')
+    await voiceChannel.connect()
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@bot.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing")
+
+
+@bot.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+@bot.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not pause.")
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+
+@bot.event()
+async def on_raw_reaction_add(payload):
+    
 
 bot.run(DISCORD_TOKEN)
