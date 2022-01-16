cd65ca849c0c3db434143c61fc33e9ba60ca21df
waltcm02 <memotexe@gmail.com>
2022-01-15 22:04:01 -0500

    Roles Fixed, Meme Commands Added

--- MemoBot.py
+++ MemoBot.py
@@ -10,49 +10,7 @@ from itertools import cycle
 from dotenv import load_dotenv
 from discord.ext.commands import Bot
 from random import choice
-
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+import random
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
@@ -76,35 +34,7 @@ async def change_status():
 @bot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='general')
-    await channel.send(f'Welcome {member.mention}!  Ready to jam out? See `!Help` command for details!')
-
-
-@bot.command(name='play', help='This command plays music')
-async def play(ctx, url):
-    if not ctx.message.author.voice:
-        await ctx.send("You are not connected to a voice channel")
-        return
-
-    else:
-        channel = ctx.message.author.voice.channel
-
-    await channel.connect()
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    async with ctx.typing():
-        player = await YTDLSource.from_url(url, loop=bot.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-    await ctx.send('**Now playing:** {}'.format(player.title))
-
-@bot.command(name='stop', help='This command stops the music and makes the bot leave the voice channel')
-async def stop(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
-
-
+    await channel.send('Welcome {member.mention} See `!Help` command for details!')
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -112,20 +42,44 @@ async def on_raw_reaction_add(payload):
     if message_id == 870847081210843167:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
-
-        if payload.emoji.name == 'mSwitch':
-            role = discord.utils.get(guild.roles, name='Switch')
-        elif payload.emoji.name == 'mPlayStation':
-            role = discord.utils.get(guild.roles, name='PlayStation')
-        elif payload.emoji.name == 'mPC':
-            role = discord.utils.get(guild.roles, name='PC')
-        elif payload.emoji.name == 'mXbox':
-            role = discord.utils.get(guild.roles, name='Xbox')
+        if payload.emoji.name == 'licker':
+            role = discord.utils.get(guild.roles, name='NSFW')
+        elif payload.emoji.name == 'chieflookup':
+            role = discord.utils.get(guild.roles, name='Halo')
+        elif payload.emoji.name == 'bob':
+            role = discord.utils.get(guild.roles, name='Runescape')
+        elif payload.emoji.name == 'minecraft_bounce':
+            role = discord.utils.get(guild.roles, name='Minecraft')
+        elif payload.emoji.name == 'aSDVstardrop':
+            role = discord.utils.get(guild.roles, name='Stardew Valley')
+        elif payload.emoji.name == 'switch':
+            role = discord.utils.get(guild.roles, name='Nintendo')
+        elif payload.emoji.name == '❤️':
+            role = discord.utils.get(guild.roles, name='Red')
+        elif payload.emoji.name == '💙':
+            role = discord.utils.get(guild.roles, name='Blue')
+        elif payload.emoji.name == '🤎':
+            role = discord.utils.get(guild.roles, name='Brown')
+        elif payload.emoji.name == '💛':
+            role = discord.utils.get(guild.roles, name='Yellow')
+        elif payload.emoji.name == '💚':
+            role = discord.utils.get(guild.roles, name='Green')
+        elif payload.emoji.name == '🤍':
+            role = discord.utils.get(guild.roles, name='White')
+        elif payload.emoji.name == '💜':
+            role = discord.utils.get(guild.roles, name='Purple')
+        elif payload.emoji.name == '🧡':
+            role = discord.utils.get(guild.roles, name='Orange')
+        elif payload.emoji.name == '🖤':
+            role = discord.utils.get(guild.roles, name='Black')
+        elif payload.emoji.name == 'pink_heart':
+            role = discord.utils.get(guild.roles, name='Pink')
         else:
             role = discord.utils.get(guild.roles, name = payload.emoji.name)
-
+       
         if role is not None:
             member = payload.member
+         
             if member is not None:
                 await member.add_roles(role)
                 print("Done")
@@ -141,19 +95,49 @@ async def on_raw_reaction_remove(payload):
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
 
-        if payload.emoji.name == 'mSwitch':
-            role = discord.utils.get(guild.roles, name='Switch')
-        elif payload.emoji.name == 'mPlayStation':
-            role = discord.utils.get(guild.roles, name='PlayStation')
-        elif payload.emoji.name == 'mPC':
-            role = discord.utils.get(guild.roles, name='PC')
-        elif payload.emoji.name == 'mXbox':
-            role = discord.utils.get(guild.roles, name='Xbox')
+        if payload.emoji.name == 'licker':
+            role = discord.utils.get(guild.roles, name='NSFW')
+        elif payload.emoji.name == 'chieflookup':
+            role = discord.utils.get(guild.roles, name='Halo')
+        elif payload.emoji.name == 'bob':
+            role = discord.utils.get(guild.roles, name='Runescape')
+        elif payload.emoji.name == 'minecraft_bounce':
+            role = discord.utils.get(guild.roles, name='Minecraft')
+        elif payload.emoji.name == 'aSDVstardrop':
+            role = discord.utils.get(guild.roles, name='Stardew Valley')
+        elif payload.emoji.name == 'switch':
+            role = discord.utils.get(guild.roles, name='Nintendo')
+        elif payload.emoji.name == '❤️':
+            role = discord.utils.get(guild.roles, name='Red')
+        elif payload.emoji.name == '💙':
+            role = discord.utils.get(guild.roles, name='Blue')
+        elif payload.emoji.name == '🤎':
+            role = discord.utils.get(guild.roles, name='Brown')
+        elif payload.emoji.name == '💛':
+            role = discord.utils.get(guild.roles, name='Yellow')
+        elif payload.emoji.name == '💚':
+            role = discord.utils.get(guild.roles, name='Green')
+        elif payload.emoji.name == '🤍':
+            role = discord.utils.get(guild.roles, name='White')
+        elif payload.emoji.name == '💜':
+            role = discord.utils.get(guild.roles, name='Purple')
+        elif payload.emoji.name == '🧡':
+            role = discord.utils.get(guild.roles, name='Orange')
+        elif payload.emoji.name == '🖤':
+            role = discord.utils.get(guild.roles, name='Black')
+        elif payload.emoji.name == 'pink_heart':
+            role = discord.utils.get(guild.roles, name='Pink')
         else:
             role = discord.utils.get(guild.roles, name = payload.emoji.name)
 
+        print("Role Removed:")
+        print(role)
+        
         if role is not None:
-            member = payload.member
+            member = await(await bot.fetch_guild(payload.guild_id)).fetch_member(payload.user_id)
+            print("Member Removed:")
+            print(member)
+            
             if member is not None:
                 await member.remove_roles(role)
                 print("Done")
@@ -163,12 +147,43 @@ async def on_raw_reaction_remove(payload):
             print("Role Not Found.")
 
 @bot.command(name='clear')
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-    await ctx.send("Deleting %s Messages" %(amount))
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int):
+    authors = {}
+    async for message in ctx.channel.history(limit=amount + 1):
+        if message.author not in authors:
+            authors[message.author] = 1
+        else:
+            authors[message.author] += 1
+        await message.delete()
 
+    msg = "I took out the trash for you!"
+    await ctx.channel.send(msg)   
+
+@bot.command(name="d20")
+async def rng(ctx):
+    randomNum = random.randint(1,20)
+    if randomNum == 1:
+        await ctx.channel.send(str(randomNum) + " Critical Fail! <a:F_:925511019655200841>")
+    elif randomNum <= 10 and randomNum > 1:
+        await ctx.channel.send(str(randomNum) + " Yikes! Thats not a good role <:tanjiroD:925511021261631528>")
+    elif randomNum >=11 and randomNum < 20:
+        await ctx.channel.send(str(randomNum) + " Ok! Thats a good role <a:gachi:926278637341245471>")
+    elif randomNum == 20:
+        await ctx.channel.send(str(randomNum) + " LETS GOO! CRITICAL ROLE! <a:gachihyperclap:890777869360431104>")
+        
+    
+@bot.command(name="sus")
+async def sus(ctx):
+    randomNum = random.randint(1,11)
+    await ctx.channel.send(file = discord.File("Sus Folder\sus"+str(randomNum)+".gif"))
     
 
-
-
+@bot.command(name="headpat")
+async def headpat(ctx):
+    randomNum = random.randint(1,11)
+    await ctx.channel.send(file = discord.File("Head Pats\hp"+str(randomNum)+".gif"))
+    
+ 
+    
 bot.run(DISCORD_TOKEN)
